2f101563803d6c0ef34f7e4aef88705b37780213
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-08-10 20:52:27 -0500

    Base LTI Config

---
+++ src/main/java/com/optimizedproductions/remote_lab_lti/Application.java
@@ -0,0 +1,16 @@
+package com.optimizedproductions.remote_lab_lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ *
+ * @author Heriberto Reyes Esparza
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String args[]){
+		SpringApplication.run(Application.class, args);
+	}
+}

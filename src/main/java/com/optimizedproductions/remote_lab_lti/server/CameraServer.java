5b1c782a81cbfc5f114700c38c643e52dfa59026
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-08-14 11:58:27 -0500

    updated camera feed method

--- src/main/java/com/optimizedproductions/remote_lab_lti/server/CameraServer.java
+++ src/main/java/com/optimizedproductions/remote_lab_lti/server/CameraServer.java
@@ -1,17 +1,16 @@
 package com.optimizedproductions.remote_lab_lti.server;
 
 
-import org.apache.commons.codec.binary.Base64;
+import com.github.sarxos.webcam.Webcam;
 import org.json.simple.JSONObject;
-import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.videoio.VideoCapture;
-import org.opencv.videoio.Videoio;
 
-import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 /**
  *
@@ -19,80 +18,64 @@ import java.util.logging.Logger;
  */
 public class CameraServer {
 
-	static {
-		System.out.println( System.getProperty("user.dir") );
-		String direct_path = System.getProperty("user.dir") + File.separator + "libs" + File.separator + Core.NATIVE_LIBRARY_NAME + ".dll";
-		System.out.println( Core.NATIVE_LIBRARY_NAME );
-		System.out.println( direct_path );
-		//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		System.load( direct_path );
-	}
-
-	public static void print_version(){
-		System.out.println("Welcome to OpenCV " + Core.VERSION);
-	}
-
 	public static void start_video_service_thread(){
-		new Thread(CameraServer::start_video_service).start();
+		new Thread(() -> {
+			CameraServer.start_video_service();
+		}).start();
 	}
 
 	public static void start_video_service(){
-
-		Mat webcamMatImage = new Mat();
-		String img_encoded;
-		VideoCapture capture = new VideoCapture(    0   );
-
-		//int w = 240, h = 180;
-		//int w = 320, h = 240;
-		int w = 400, h = 320;
-
-		MatOfInt jpeg_params = new MatOfInt(Imgcodecs.IMWRITE_JPEG_QUALITY, 90);
-
-		capture.set(Videoio.CAP_PROP_FRAME_WIDTH    ,w);
-		capture.set(Videoio.CAP_PROP_FRAME_HEIGHT   ,h);
-
-		JSONObject data = new JSONObject();
-		data.put("data", "");
-		data.put("type",MyWebSocketServer.DATA_TYPE_IMG_UPDATE);
-		if( capture.isOpened()){
-			while (true){
-				capture.read(webcamMatImage);
-				if( !webcamMatImage.empty() ){
-					Size sz = new Size(w,h);
-
-					Imgproc.resize(webcamMatImage, webcamMatImage, sz);
-
-					MatOfByte buffer = new MatOfByte();
-					Imgcodecs.imencode(".jpg", webcamMatImage, buffer, jpeg_params);
-					byte[] return_buff = buffer.toArray();
-
-					StringBuilder sb = new StringBuilder();
-					sb.append("data:image/jpeg;base64,");
-					sb.append( Base64.encodeBase64String(return_buff)  );
-					img_encoded = sb.toString();
-					//System.out.print( "." );
-					data.put("data", img_encoded);
-					if(
-							MyWebSocketServer.getInstance().sendToAll( data )
-							){
-						//
-					}
-					try {
-						//Thread.sleep(34);
-						//Thread.sleep(12);
-					} catch (Exception ex) {
-						Logger.getLogger(CameraServer.class.getName()).log(Level.SEVERE, null, ex);
-					}
-				}
-				else{
-					System.out.println(" -- Frame not captured -- Break!");
-					break;
-				}
+		Webcam webcam = Webcam.getDefault();
+		try {
+			List<Webcam> my_cams = Webcam.getWebcams(1000);
+			for ( Webcam cam : my_cams ){
+				System.out.println("MyCams Starts");
+				System.out.println(">> " + cam.getName());
+				System.out.println("MyCams Ends");
+				if( cam.getName().contains("C170") )
+					webcam = cam;
 			}
+		} catch (TimeoutException e) {
+			e.printStackTrace();
 		}
-		else{
-			System.out.println("Couldn't open capture.");
+		String img_encoded;
+
+		try {
+			webcam.open();
+
+			JSONObject data = new JSONObject();
+			data.put("data", "");
+			data.put("type",MyWebSocketServer.DATA_TYPE_IMG_UPDATE);
+
+			while(webcam.isOpen() ){
+				BufferedImage im = webcam.getImage();
+
+				//
+
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				ImageIO.write(im, "PNG", out);
+				String img_base64 = DatatypeConverter.printBase64Binary(out.toByteArray());
+
+
+				StringBuilder sb = new StringBuilder();
+				sb.append("data:image/jpeg;base64,");
+				sb.append( img_base64 );
+				img_encoded = sb.toString();
+				//System.out.print( "." );
+				data.put("data", img_encoded);
+				if(
+						MyWebSocketServer.getInstance().sendToAll( data )
+						){
+					//
+				}
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+
 	}
 
 }
